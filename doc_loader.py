68b7a42494f8a2f47cdee6202692273fc5280b54
jayiuk <jayiuk987@gmail.com>
2024-09-24 16:21:42 +0900

    coding

--- doc_loader.py
+++ doc_loader.py
@@ -5,18 +5,8 @@ from langchain_community.document_loaders import SeleniumURLLoader
 
 
 class document_loader():
-    def __init__(self, df):
-        self.df = df
-    
-    def url_load(self):
-        df = self.df
-        urls = []
-        for url in df['link']:
-            urls.append(url)
-        return urls
-    
+    def __init__(self, list):
+        self.list = list
+
     def url2doc(self):
-        urls = self.url_load()
-        sul = SeleniumURLLoader(urls = urls, browser = 'chrome', continue_on_failure = True, headless = True)
-        url_docs = sul.load_and_split()
-        return url_docs
+        
