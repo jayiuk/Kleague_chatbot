1152231ac097d93b12ddcbd6b90a5b8b833414c9
jayiuk <jayiuk987@gmail.com>
2024-09-24 10:27:27 +0900

    kleague news crawling done

--- crawling.py
+++ crawling.py
@@ -1,37 +1,40 @@
 import pandas as pd
-import numpy as np
-import os
-import urllib.request
-import json
+import requests
+from bs4 import BeautifulSoup
 
 class get_news():
     def __init__(self, url):
         self.url = url
     
     def get_kleague_news(self):
-        client_id = os.getenv('naver_api_id')
-        client_secret = os.getenv('naver_api_pw')
-        encText = urllib.parse.quote('k리그')
-        display = 100
-        url = self.url + encText + '&display=' + str(display) + '&sort=date'
-        request = urllib.request.Request(url)
-        request.add_header("X-Naver-Client-Id", client_id)
-        request.add_header('X-Naver-Client-Secret', client_secret)
-        response = urllib.request.urlopen(request)
-        rescode = response.getcode()
-        if (rescode == 200):
-            response_body = response.read()
-            res = response_body.decode('utf-8')
-            response_fin = json.loads(res)
-            return response_fin
-        else:
-            print("Error Code : ", rescode)
-    
-    def to_df(self, response):
-        res = response['items']
-        df = pd.DataFrame(columns = ['title', 'link', 'description', 'pubdate'])
-        df_list = []
-        for data in res:
-            df_list.append(data)
-        df_con = pd.DataFrame(df_list)
-        return df_con
+        base_url = self.url
+        page = 1
+        news_list = []
+        while True:
+            print(f"크롤링 중인 페이지 : {page}")
+            url = base_url + '&page=' + str(page)
+            response = requests.get(url)
+            if response.status_code != 200:
+                print("크롤링 종료")
+                break
+            soup = BeautifulSoup(response.text, 'html.parser')
+            news_elements = soup.select('ul.thum-list.f-wrap a')
+            if not news_elements:
+                print("크롤링 끝")
+                break
+            for news in news_elements:
+                link = news.get('href')
+                if link:
+                    real_url = f"https://www.kleague.com{link}"
+                    if real_url in news_list:
+                        print("크롤링 끝")
+                        break
+                    news_list.append(real_url)
+                    
+            page += 1
+        return news_list
+
+    def get_df(self):
+        news_list = self.get_kleague_news()
+        df = pd.DataFrame({'link' : news_list})
+        return df
