27de2cb3c50b749a18df3c2d586145341cd10a8d
jayiuk <jayiuk987@gmail.com>
2024-09-27 17:41:02 +0900

    customizing RecursiveCharacterTextSplitter

--- splitter.py
+++ splitter.py
@@ -2,15 +2,13 @@ import langchain
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 class split(RecursiveCharacterTextSplitter):
-    def __init__(self, doc_list, overlap, chunk_size):
-        super().__init__(overlap = overlap, chunk_size = chunk_size)
+    def __init__(self, doc_list, overlap, chunk_size = None, **kwargs):
+        super().__init__(overlap = overlap, chunk_size = chunk_size, **kwargs)
         self.doc_list = doc_list
-    
-    def get_doc_len(self):
-        l_list = [len(doc) for doc in self.doc_list]
-        return l_list
 
     def split_text(self):
-        l_list = self.get_doc_len()
-        doc_list = self.doc_list
-        
+        chunked_list = []
+        for doc in self.doc_list:
+            splitted_doc = doc.split("\n\n")
+            self._chunk_size = len(splitted_doc)
+            
