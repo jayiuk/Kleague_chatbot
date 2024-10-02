03f3726614ca9eeab6155e547b2f791cf6a2ec31
jayiuk <jayiuk987@gmail.com>
2024-10-02 16:49:08 +0900

    customizing RecursiveCharacterTextSplitter

--- splitter_demo.py
+++ splitter_demo.py
@@ -8,15 +8,18 @@ class split(RecursiveCharacterTextSplitter):
 
     def split_text(self):
         chunked_list = []
-        chunked_doc_list = []
         doc_list = [doc.page_content for doc in self.doc_list]
         for doc in doc_list:
             splitted_doc = doc.split('\n\n')
             self.chunk_size = len(splitted_doc)
             self.chunk_overlap = self.chunk_size // 3
-            for splitted_doc in doc:
-                chunked_doc = splitted_doc.split('.')
-                chunked_doc_list.append(chunked_doc)
-            for _ in range()
+            chunked_list.append(splitted_doc)
+            for _ in splitted_doc:
+                overlaped = splitted_doc[-self.chunk_overlap:]
+                chunked = overlaped + splitted_doc
+                if splitted_doc[0] == True:
+                    chunked = splitted_doc
+                chunked_list.append(chunked)
+
         return chunked_list
                     
